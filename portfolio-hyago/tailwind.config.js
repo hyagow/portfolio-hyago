20e588b1a06600da760c87fd3a1f7c4b19f80c88
hyagow <hyagosdj@gmail.com>
2025-11-30 19:55:21 -0300

    Reapply "feat: inserted a all project of portfolio-hyago"
    This reverts commit 2a2c52e1a5e5d760958c5fd57cb77aebb82e3f35.

---
+++ portfolio-hyago/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // CRÍTICO: Inclui todos os seus arquivos de código
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
